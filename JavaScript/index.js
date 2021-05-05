d266d9ed38fe7369b1836ea9f6edd7a3f33552aa
Irenevs83 <80913299+Irenevs83@users.noreply.github.com>
2021-05-05 15:37:08 +0200

    Add files via upload

---
+++ JavaScript/index.js
@@ -0,0 +1,13 @@
+console.log("Hello Winc Academy");
+let firstName = "Irene";
+console.log(firstName);
+// je kunt geen variable maken met if omdat dit een reserved keyword is.//
+console.log(4 + 4);
+console.log("4" + "4");
+// 2 strings kun je niet bij elkaar optellen. Deze plaatst de cijfers naast elkaar.//
+console.log(4 - 2);
+console.log(4 * 2);
+console.log(4 / 2);
+console.log(4 % 2);
+console.log(typeof 37);
+console.log(typeof zevenendertig);
